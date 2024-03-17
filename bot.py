774c1559d3ad30c24e6869c3999006496dd29167
rcchcz <rcchcz@gmail.com>
2024-03-17 10:55:19 -0300

    feat: add more answers

--- bot.py
+++ bot.py
@@ -3,7 +3,16 @@ from chatterbot import ChatBot
 
 bot = ChatBot('ChatBot')
 
-conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python']
+conversa = ['Oi',
+            'Olá',
+            'Tudo bem?',
+            'Tudo ótimo',
+            'Você gosta de programar?',
+            'Sim, eu programo em Python',
+            'Que tipos de programas você gosta de criar?',
+            'Eu gosto de criar programas que ajudem as pessoas em suas tarefas diárias.',
+            'Foi ótimo conversar com você.',
+            'Foi ótimo conversar com você também. Tenha um ótimo dia!']
 
 trainer = ListTrainer(bot)
 trainer.train(conversa)
