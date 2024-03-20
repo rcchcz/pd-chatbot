ad1b0fbc567eb546f7d5c9c235d37f3124b76dcf
Antony Lemos <djalma.rodrigues@luizalabs.com>
2024-03-20 11:29:08 -0300

    feat: add rpc client

---
+++ rpc/client.py
@@ -0,0 +1,18 @@
+import xmlrpc.client
+
+
+def rpc_client():
+    server = xmlrpc.client.ServerProxy("http://localhost:8000")
+
+    message = input(" -> ")
+
+    while message.lower().strip() != 'bye':
+        server_response = server.answer(message)
+
+        print('Received from server: ' + server_response)
+
+        message = input(" -> ")
+
+
+if __name__ == "__main__":
+    rpc_client()
